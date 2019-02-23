ca468f79b632ca8bf4bf93c050dfdf549d9f1b8b
LeeKunLin <LeeKunLin@LeeKunLins-MBP.local>
2019-02-23 17:00:10 +0800

    display home

--- Python/Flask_Blog/_practice/flask_blog.py
+++ Python/Flask_Blog/_practice/flask_blog.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 posts = [
     {
-        'author': 'Corey Schafer',
-        'tiｖtle': 'Blog Post 1',
+        'author': 'Kevin Lee',
+        'title': 'Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
+        'author': 'Kevin Lee',
+        'title': 'Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
     }
@@ -22,9 +22,13 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('about.html', posts=posts)
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
     return render_template('about', title='About')
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port="5000", debug=True)
