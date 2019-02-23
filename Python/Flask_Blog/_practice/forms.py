20ae38f9e254b88b2f895ecc30cc21040e3028aa
LeeKunLin <LeeKunLin@LeeKunLins-MBP.local>
2019-02-23 17:45:35 +0800

    RegistrationForm init

---
+++ Python/Flask_Blog/_practice/forms.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=10)])
+    email = StringField('Email', validators=[Email()])
+    password = PasswordField('Password', validators=[DataRequired])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign up')
